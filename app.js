ffca036d5d2f6eba4aa2a485c1b1aacc244c0c44
AdetolaAdebola <36213634+deytola@users.noreply.github.com>
2018-10-24 14:47:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import routes from './server/routes/index';
 
 const port = process.env.port || 4000; // port binding
 const app = express();
@@ -10,7 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //import routes into the application
-require('./server/routes')(app);
+app.use('/', routes);
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
